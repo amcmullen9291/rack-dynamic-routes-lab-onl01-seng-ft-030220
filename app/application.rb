8d658cebde77c16bad40f8726315d123ddb289b5
anthony mcmullen <jondouglasheit@gmail.com>
2020-05-05 11:10:09 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,8 +6,8 @@ class Application
     req = Rack::Request.new(env)
     if req.path.match(/items/)
   	  item_name = req.path.split("/item/").last 
-  	  item = @@items.find{|s| s.title == song_title}
-      resp.write song.artist
+  	  item = @@items.find{|s| s.name == item_name}
+      resp.write item_name.name
     end
   	 
     resp.finish
