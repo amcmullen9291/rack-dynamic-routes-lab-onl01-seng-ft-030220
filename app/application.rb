35520e77f9270efd3fc62fbce88c9e65f61d60a6
anthony mcmullen <jondouglasheit@gmail.com>
2020-05-05 11:05:35 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,10 +6,9 @@ Song.new("Hello","Adele")]
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-  if req.path.match(/songs/)
-  	 
-  	song_title = req.path.split("/songs/").last #turn /songs/Sorry into Sorry
-  	song = @@songs.find{|s| s.title == song_title}
+    if req.path.match(/songs/)
+  	  item_name = req.path.split("/item/").last 
+  	  item = @@songs.find{|s| s.title == song_title}
       resp.write song.artist
     end
   	 
