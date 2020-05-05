2b1ed272ba6ae0625607eec11fe790f6dac8976d
anthony mcmullen <jondouglasheit@gmail.com>
2020-05-05 11:11:32 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,11 @@ class Application
     if req.path.match(/items/)
   	  item_name = req.path.split("/item/").last 
   	  item = @@items.find{|s| s.name == item_name}
-      resp.write item_name.name
+  	  resp.write item.name " is:"
+      resp.write item.price
+      resp.status = 200
+    else
+      
     end
   	 
     resp.finish
